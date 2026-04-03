037876ca789b4b3700d5bbb77f31ebc38964f63b
khael-apus <119275217+khael-apus@users.noreply.github.com>
2026-04-03 20:55:58 +0800

    updated main.py :: change font
    change font to Inter

--- verifid/main.py
+++ verifid/main.py
@@ -1,10 +1,15 @@
 import sys
+from PySide6.QtGui import QFont
 from PySide6.QtWidgets import QApplication
 from ui.main_window import MainWindow
 
 
 def main():
     app = QApplication(sys.argv)
+
+    font = QFont("Inter")
+    app.setFont(font)
+    
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
